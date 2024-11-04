5e38806f32ff938c0995208c8bda97c2f6839e7f
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-11-05 02:59:36 +0600

    navbar and hero section done

---
+++ src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,10 @@
+
+const DashBoard = () => {
+    return (
+        <div>
+            <h2 className="4xl">Dashboard</h2>
+        </div>
+    );
+};
+
+export default DashBoard;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="5xl">Page Not Found</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
